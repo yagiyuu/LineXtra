0393661d107608b31aafb06d77bac1d89c409252
ゆう <184002616+yagiyuu@users.noreply.github.com>
2024-10-11 23:41:18 +0900

    removed hide tabs

--- app/src/main/java/io/github/yagiyuu/linextra/Main.kt
+++ app/src/main/java/io/github/yagiyuu/linextra/Main.kt
@@ -1,9 +1,6 @@
 package io.github.yagiyuu.linextra
 
-import android.app.Activity
-import android.content.res.Resources
 import android.view.View
-import android.view.ViewGroup
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -14,9 +11,6 @@ import io.github.yagiyuu.linextra.Util.hideView
 class Main : IXposedHookLoadPackage {
     companion object {
         private const val PACKAGE_NAME = "jp.naver.line.android"
-
-        // bnb_timeline=VOOM, bnb_news=News, bnb_wallet=Wallet, bnb_home=Home, bnb_chat=Talk
-        private val HIDDEN_TAB_IDS = arrayOf("bnb_timeline", "bnb_news", "bnb_wallet")
     }
 
     override fun handleLoadPackage(loadPackageParam: LoadPackageParam) {
@@ -47,38 +41,6 @@ class Main : IXposedHookLoadPackage {
                     val view = param.thisObject as? View ?: return
                     if (view.visibility != View.GONE) {
                         view.hideView()
-                    }
-                }
-            }
-        )
-
-        XposedBridge.hookAllMethods(
-            XposedHelpers.findClass(
-                "jp.naver.line.android.activity.main.MainActivity",
-                loadPackageParam.classLoader
-            ),
-            "onCreate",
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val activity = param.thisObject as? Activity ?: return
-                    val tabContainer = activity.findViewById<ViewGroup>(
-                        activity.resources.getIdentifier(
-                            "main_tab_container",
-                            "id",
-                            PACKAGE_NAME
-                        )
-                    ) ?: return
-
-                    for (i in 0 until tabContainer.childCount) {
-                        val childView = tabContainer.getChildAt(i)
-                        val name = try {
-                            activity.resources.getResourceEntryName(childView.id)
-                        } catch (_: Resources.NotFoundException) {
-                            continue
-                        }
-                        if (HIDDEN_TAB_IDS.any { name.startsWith(it) }) {
-                            childView.hideView()
-                        }
                     }
                 }
             }
